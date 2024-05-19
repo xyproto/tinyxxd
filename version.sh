a5824784408e070d2f428d0d4920c400d84f012e
Alexander F. Rødseth <rodseth@gmail.com>
2024-05-19 13:53:04 +0200

    New patch release

--- version.sh
+++ version.sh
@@ -4,7 +4,7 @@
 #
 
 # The current version goes here, as the default value
-VERSION=${1:-'1.3.2'}
+VERSION=${1:-'1.3.3'}
 
 if [ -z "$1" ]; then
   echo "The current version is $VERSION, pass the new version as the first argument if you wish to change it"
@@ -13,4 +13,3 @@ fi
 
 echo "Setting the version to $VERSION"
 sed -i "s/tinyxxd [[:digit:]]*\.[[:digit:]]*\.[[:digit:]]*/tinyxxd $VERSION/g" main.c
-sed -i "s/[[:digit:]]*\.[[:digit:]]*\.[[:digit:]]*/$VERSION/g" release.sh README.md "$0"
