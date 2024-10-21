9cca34d9277cb060fe3d60b6e1205d35e97c43b2
Alexander F. Rødseth <rodseth@gmail.com>
2024-10-21 12:44:25 +0200

    Update the version script

--- version.sh
+++ version.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 # The current version goes here, as the default value
-VERSION=${1:-'1.3.5'}
+VERSION=${1:-'1.3.6'}
 
 if [ -z "$1" ]; then
   echo "The current version is $VERSION, pass the new version as the first argument if you wish to change it"
@@ -10,3 +10,4 @@ fi
 echo "Setting the version to $VERSION"
 sed -i "s/tinyxxd [[:digit:]]*\.[[:digit:]]*\.[[:digit:]]*/tinyxxd $VERSION/g" main.c
 sed -i "s/VERSION ?= [[:digit:]]*\.[[:digit:]]*\.[[:digit:]]*/VERSION ?= $VERSION/g" Makefile
+sed -i "s/tinyxxd [[:digit:]]*\.[[:digit:]]*\.[[:digit:]]*/tinyxxd $VERSION/g" tinyxxd.1
