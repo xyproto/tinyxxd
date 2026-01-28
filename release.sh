83df3ca25f110d7bf338060dffe64113825dd9e0
Alexander F. Rødseth <rodseth@gmail.com>
2026-01-28 15:54:59 +0100

    Update version + release scripts

---
+++ release.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+VERSION=$(grep VERSION version.sh | head -1 | cut -d"'" -f2)
+if [[ $VERSION == "" ]]; then
+  echo 'no version'
+  exit 1
+fi
+mkdir -p release/tinyxxd-$VERSION
+cp -v AUTHORS.md LICENSE main.c Makefile README.md tinyxxd.1 release/tinyxxd-$VERSION
+cd release
+tar zcvf tinyxxd-$VERSION.tar.gz tinyxxd-$VERSION
+cd ..
