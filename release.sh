bc36d44ec46d2cd6ec68917997bc2dfd279d450e
Alexander F. Rødseth <rodseth@gmail.com>
2026-02-13 09:07:57 +0100

    Update the release script

--- release.sh
+++ release.sh
@@ -8,4 +8,5 @@ mkdir -p release/tinyxxd-$VERSION
 cp -v AUTHORS.md LICENSE main.c Makefile README.md tinyxxd.1 release/tinyxxd-$VERSION
 cd release
 tar zcvf tinyxxd-$VERSION.tar.gz tinyxxd-$VERSION
+tar Jcvf tinyxxd-$VERSION.tar.xz tinyxxd-$VERSION
 cd ..
