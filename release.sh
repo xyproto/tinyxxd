aef66521932fe5d4d4d8e5a81d485fe4f4a3c920
Alexander F. Rødseth <rodseth@gmail.com>
2026-02-17 18:38:29 +0100

    Add more quotes

--- release.sh
+++ release.sh
@@ -4,9 +4,9 @@ if [[ $VERSION == "" ]]; then
   echo 'no version'
   exit 1
 fi
-mkdir -p release/tinyxxd-$VERSION
-cp -v AUTHORS.md LICENSE main.c Makefile README.md tinyxxd.1 release/tinyxxd-$VERSION
+mkdir -p "release/tinyxxd-$VERSION"
+cp -v AUTHORS.md LICENSE main.c Makefile README.md tinyxxd.1 "release/tinyxxd-$VERSION"
 cd release
-tar zcvf tinyxxd-$VERSION.tar.gz tinyxxd-$VERSION
-tar Jcvf tinyxxd-$VERSION.tar.xz tinyxxd-$VERSION
+tar zcvf "tinyxxd-$VERSION.tar.gz" "tinyxxd-$VERSION"
+tar Jcvf "tinyxxd-$VERSION.tar.xz" "tinyxxd-$VERSION"
 cd ..
